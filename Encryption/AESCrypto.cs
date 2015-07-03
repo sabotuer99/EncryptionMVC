ed9a25752163393c81031350245a6b7bb3f79633
sabotuer99 <sabotuer99@gmail.com>
2015-07-02 21:10:39 -1200

    Finally got the homebrew method working

---
+++ Encryption/AESCrypto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace Encryption
+{
+    public class AESCrypto
+    {
+        byte[] Key = { 123, 217, 19, 11, 24, 26, 85, 45, 114, 184, 27, 162, 37, 112, 222, 209, 241, 24, 175, 144, 173, 53, 196, 29, 24, 26, 17, 218, 131, 236, 53, 209 };
+        byte[] IV = { 146, 64, 191, 111, 23, 3, 113, 119, 231, 121, 221, 112, 79, 32, 114, 156 };
+
+
+        public string Encrypt(string userID)
+        {
+            using (var cryptoProvider = new AesCryptoServiceProvider())
+            using (var memoryStream = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateEncryptor(Key, IV), CryptoStreamMode.Write))
+            using (var writer = new StreamWriter(cryptoStream))
+            {
+                writer.Write(userID);
+                writer.Flush();
+                cryptoStream.FlushFinalBlock();
+                //writer.Flush();
+                return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+            }
+        }
+
+
+        public string Decrypt(string userID)
+        {
+            using (var cryptoProvider = new AesCryptoServiceProvider())
+            using (var memoryStream = new MemoryStream(Convert.FromBase64String(userID)))
+            using (var cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+            using (var reader = new StreamReader(cryptoStream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- Encryption/Controllers/HomeController.cs
+++ Encryption/Controllers/HomeController.cs
@@ -35,7 +35,14 @@ namespace Encryption.Controllers
         {
             SimplerAES saes = new SimplerAES();
             var cipherText = saes.Encrypt(plainText);
-            return cipherText;
+            return cipherText + " | " + saes.Decrypt(cipherText);
+        }
+
+        public String EncryptToo(String plainText)
+        {
+            AESCrypto aes = new AESCrypto();
+            var cipherText = aes.Encrypt(plainText);
+            return cipherText + " | " + aes.Decrypt(cipherText);
         }
 
         public String StreamEncrypt(String plainText)
@@ -43,10 +50,11 @@ namespace Encryption.Controllers
             var textStream = new MemoryStream();
 
             RijndaelManaged RMCrypto = new RijndaelManaged();
+            RMCrypto.Mode = CipherMode.CBC;
             RMCrypto.Padding = PaddingMode.ISO10126;
 
-            byte[] Key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
-            byte[] IV = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+            byte[] Key = { 123, 217, 19, 11, 24, 26, 85, 45, 114, 184, 27, 162, 37, 112, 222, 209, 241, 24, 175, 144, 173, 53, 196, 29, 24, 26, 17, 218, 131, 236, 53, 209 };
+            byte[] IV = { 146, 64, 191, 111, 23, 3, 113, 119, 231, 121, 221, 112, 79, 32, 114, 156 };
 
             //Create a CryptoStream, pass it the NetworkStream, and encrypt 
             //it with the Rijndael class.
@@ -57,39 +65,45 @@ namespace Encryption.Controllers
             //Create a StreamWriter for easy writing to the 
             //network stream.
             StreamWriter SWriter = new StreamWriter(CryptStream);
+            SWriter.WriteLine(plainText);
             SWriter.Flush();
             CryptStream.FlushFinalBlock();
-            textStream.Position = 0;
 
             var encrypted = Convert.ToBase64String(textStream.GetBuffer(), 0, (int)textStream.Length);
 
-            //var encStream = new MemoryStream(Convert.FromBase64String(encrypted));
+            var encStream = new MemoryStream(Convert.FromBase64String(encrypted));
 
-            //CryptoStream DecryptStream = new CryptoStream(textStream, 
-            //RMCrypto.CreateDecryptor(Key, IV), 
-            //CryptoStreamMode.Read);
+            //THIS WORKS
+            //CryptoStream DecryptStream = new CryptoStream(encStream, 
+            //    RMCrypto.CreateDecryptor(Key, IV), 
+            //    CryptoStreamMode.Read);
 
-            //StreamReader SReader = new StreamReader(DecryptStream);
+            textStream.Position = 0;
+            CryptoStream DecryptStream = new CryptoStream(textStream,
+                RMCrypto.CreateDecryptor(Key, IV),
+                CryptoStreamMode.Read);
+
+            StreamReader SReader = new StreamReader(DecryptStream);
+            string result = SReader.ReadToEnd();
 
             //var decrypted = SReader.ReadToEnd();
-            string result;
-            using (var input = new MemoryStream(Convert.FromBase64String(encrypted)))
-            using (var output = new MemoryStream())
-            {
-                var decryptor = RMCrypto.CreateDecryptor(Key, IV);
-                using (var cryptStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read))
-                {
-                    var buffer = new byte[1024];
-                    var read = cryptStream.Read(buffer, 0, buffer.Length);
-                    while (read > 0)
-                    {
-                        output.Write(buffer, 0, read);
-                        read = cryptStream.Read(buffer, 0, buffer.Length);
-                    }
-                    cryptStream.Flush();
-                    result = Encoding.Unicode.GetString(output.ToArray());
-                }
-            }
+            //using (var input = new MemoryStream(Convert.FromBase64String(encrypted)))
+            //using (var output = new MemoryStream())
+            //{
+            //    var decryptor = RMCrypto.CreateDecryptor(Key, IV);
+            //    using (var cryptStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read))
+            //    {
+            //        var buffer = new byte[1024];
+            //        var read = cryptStream.Read(buffer, 0, buffer.Length);
+            //        while (read > 0)
+            //        {
+            //            output.Write(buffer, 0, read);
+            //            read = cryptStream.Read(buffer, 0, buffer.Length);
+            //        }
+            //        cryptStream.Flush();
+            //        result = Encoding.Unicode.GetString(output.ToArray());
+            //    }
+            //}
 
             return encrypted + "  |  " + result;
         }
